2ca287671f569533266b91c7dc780f3f84f4d6bd
VicRmzChz <vicentermzchz@gmail.com>
2022-01-06 15:36:31 -0600

    Code for Edit, delete, get item and show all items add it

--- app.js
+++ app.js
@@ -4,11 +4,14 @@
 'use strict';
 const express = require('express');
 const mysql = require('mysql');
+const  cors = require('cors');
 
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-// Conexio a la DB
+// Connection DB info
 const conexio = mysql.createConnection({
     host: 'localhost',
     user: '',
@@ -17,6 +20,7 @@ const conexio = mysql.createConnection({
     port: '8889'
 });
 
+// Connect to DB
 conexio.connect((error)=> {
     if (error){
         throw error;
@@ -31,7 +35,7 @@ app.get('/', (req, res) =>{
 });
 
 
-// Show all articuls
+// Show all items
 app.get('/api/articulos', (req, res)=>{
     conexio.query('SELECT * FROM articulos', (err, results)=>{
         if (err){
@@ -42,7 +46,7 @@ app.get('/api/articulos', (req, res)=>{
     })
 });
 
-// Show only one articul
+// Show only one item
 app.get('/api/articulos/:id', (req, res)=>{
     conexio.query('SELECT * FROM articulos WHERE id = ?', [req.params.id], (err, result)=>{
         if (err){
@@ -53,6 +57,52 @@ app.get('/api/articulos/:id', (req, res)=>{
     })
 });
 
+// create a item
+app.post('/api/articulos', (req, res)=>{
+    let data = {
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        stock: req.body.stock
+    };
+
+    let sql = "INSERT INTO articulos SET ?";
+    conexio.query(sql, data, (err, results)=> {
+        if (err){
+            throw err;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+//edit item
+app.put('/api/articulos/:id', (req, res)=>{
+    let id = req.params.id;
+    let descripcion = req.body.descripcion;
+    let precio = req.body.precio;
+    let stock = req.body.stock;
+    let sql = 'UPDATE articulos SET descripcion = ?, precio = ?, stock = ? WHERE id = ?';
+    conexio.query(sql, [descripcion, precio, stock, id], (err, result)=>{
+        if (err){
+            throw err;
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+// delete a item
+app.delete('/api/articulos/:id', (req, res)=>{
+    conexio.query('DELETE FROM articulos WHERE id = ?', [req.params.id], (err, result)=>{
+        if (err){
+            throw err;
+        } else{
+            res.send(result);
+        }
+    });
+});
+
+// Port
 app.listen('3000', () =>{
     console.log('Server Running OK');
 });
